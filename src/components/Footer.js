83b210760ee1b11b23755787a5110bcad4a840fa
Styrlingjohn <styrling_morris@yahoo.com>
2023-11-28 12:28:14 -0700

    added contact info and organized footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,15 @@ import React from "react"
 function Footer() {
     return (
         <div className="footer">
-            <br/>
-            <section className="FBLogoHolder">
-                <a href="https://www.facebook.com/MorrisConstructioninc" className="FBLogoHolder" >
-                    <img className='fbLogo' alt="FB" src="/FB.png" />
+
+            <section className="contactInfo">
+                <h1> Contact Info: </h1>
+                <a href="tel:208-339-1132"> 208-339-1132 </a>
+                <a href="mailto:k.morris32@yahoo.com"> k.morris32@yahoo.com </a>
+            </section>
+            <section className="FBLogoS">
+                <a href="https://www.facebook.com/MorrisConstructioninc" className="FBLogoHolder">
+                    <img className='fbLogo' alt="FB" src="/FB.png"/>
                 </a>
             </section>
         </div>
